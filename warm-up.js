14243b9470185173be40b2c580d6ce4a77a0199a
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-07 12:36:47 -0500

    Define variables materials and words

--- warm-up.js
+++ warm-up.js
@@ -4,3 +4,10 @@ var apples = 14;
 console.log(apples);
 
 console.log('I have ' + apples + ' apples.');
+
+var materials = ['wood', 'metal', 'stone'];
+var words = {
+  'elephant': "The world's largest animal.",
+  'school': 'A place for learning.',
+  'ice cream': 'A delicious milk based dessert.'
+}
